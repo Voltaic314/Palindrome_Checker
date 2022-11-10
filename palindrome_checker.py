0d62d0da08a20288e566b77bf0d5acb2a849c98b
Logan Maupin <104940692+Voltaic314@users.noreply.github.com>
2022-11-09 19:47:57 -0500

    Add files via upload

---
+++ palindrome_checker.py
@@ -0,0 +1,48 @@
+"""
+Author: Logan Maupin
+Date: 11/09/2022 (MM/DD/YYYY)
+
+This is a simple program that checks if a given string is a palindrome.
+A palindrome is a word that is spelled the same both forwards and backwards. For example, the
+word "racecar", is a palindrome.
+"""
+
+
+def palindrome_test(string: str):
+
+    lower_case_string = string.lower()
+
+    if string:
+
+        for character in lower_case_string:
+
+            if not str(character).isalpha():
+                print(f"{string.title()} is not acceptable. Please input a string without characters or digits.")
+                break
+
+            else:
+
+                # This is the part of the code that reverses the string.
+                # After reversing the string, make sure it's the same as the original lower case enforced string.
+                if lower_case_string[::-1] == lower_case_string:
+                    print(f"{string.title()} is a palindrome.")
+                    return True
+
+                else:
+                    print(f"{string.title()} is not a palindrome.")
+                    return False
+
+    else:
+        print("String argument can not be empty. Please run this function again with a non empty string.")
+        return False
+
+
+def main():
+    palindrome_test("")
+    palindrome_test("Racecar123")
+    palindrome_test("Test")
+    palindrome_test("Racecar")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_palindrome_checker.py
@@ -0,0 +1,21 @@
+"""
+Author: Logan Maupin
+Date: 11/09/2022
+
+This is my first testing script I've ever written, and it's pretty fun! I could get used to this1
+I still have so much to learn in Python, but I'm really proud of how far I've come so far. I can't wait
+to continue my journey forward with this language and other programming languages.
+"""
+
+
+import unittest
+from palindrome_checker import palindrome_test
+
+
+class TestPalindrome(unittest.TestCase):
+
+    def test_palindrome_test(self):
+        self.assertEqual(True, palindrome_test("racecar"))
+        self.assertEqual(True, palindrome_test("Racecar"))
+        self.assertEqual(False, palindrome_test("Racecar123"))
+        self.assertEqual(False, palindrome_test(""))
